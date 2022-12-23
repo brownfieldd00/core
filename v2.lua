bd442e9965a06df43a2c090eddac6102749cd527
David Brownfield <brownfieldd00@gmail.com>
2022-12-23 18:16:20 -0500

    Create v2.lua

---
+++ v2.lua
@@ -0,0 +1,12 @@
+local core = {}
+core.__index = core
+core.Modules = {}
+
+function core:awaitGameReady()
+    if not game:IsLoaded() then game.Loaded:Wait() end
+    return true
+end
+
+function core:httpG(url)
+    return syn.request({Url = url, Method = 'GET'}).Body
+end
